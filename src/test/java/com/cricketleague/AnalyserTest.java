c7f94677350065a9584275129e04fe187d4c2356
janhavi07 <partejanhavi@gmail.com>
2019-12-14 18:07:02 +0530

    Added test code to get the player with high striking rate

--- src/test/java/com/cricketleague/AnalyserTest.java
+++ src/test/java/com/cricketleague/AnalyserTest.java
@@ -6,18 +6,18 @@ import org.junit.Test;
 
 public class AnalyserTest {
 
-    private static final String RUNS_FILE="./src/test/resources/IPL2019FactsheetMostRuns.csv";
-    private static final String WRONG_FILE="./src/test/resources/IPL2019FactsheetRuns.csv";
-    private static final String INCORRECT_DELIMITER_FILE="./src/test/resources/IncorrectDelimiterIPL2019FactsheetMostRuns.csv";
+    private static final String RUNS_FILE = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
+    private static final String WRONG_FILE = "./src/test/resources/IPL2019FactsheetRuns.csv";
+    private static final String INCORRECT_DELIMITER_FILE = "./src/test/resources/IncorrectDelimiterIPL2019FactsheetMostRuns.csv";
 
 
     @Test
     public void givenTheRunsFile_ShouldReturnTheTotalCountOfProgram() {
-        CricketLeagueAnalyser cricketLeagueAnalyser=new CricketLeagueAnalyser();
-        int getCount= 0;
+        CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
+        int getCount = 0;
         try {
             getCount = cricketLeagueAnalyser.getCount(RUNS_FILE);
-            Assert.assertEquals(101,getCount);
+            Assert.assertEquals(101, getCount);
         } catch (CricketLeagueException e) {
             e.printStackTrace();
         }
@@ -25,36 +25,48 @@ public class AnalyserTest {
 
     @Test
     public void givenWrongFile_ShouldThrowException() {
-        CricketLeagueAnalyser cricketLeagueAnalyser=new CricketLeagueAnalyser();
+        CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
         try {
             cricketLeagueAnalyser.getCount(WRONG_FILE);
         } catch (CricketLeagueException e) {
-            Assert.assertEquals(CricketLeagueException.ExceptionType.FILE_PROBLEM,e.type);
+            Assert.assertEquals(CricketLeagueException.ExceptionType.FILE_PROBLEM, e.type);
         }
     }
 
     @Test
     public void givenFileWithIncorrectDelimiter_ShouldThrowException() {
-        CricketLeagueAnalyser cricketLeagueAnalyser=new CricketLeagueAnalyser();
+        CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
         try {
             cricketLeagueAnalyser.getCount(INCORRECT_DELIMITER_FILE);
         } catch (CricketLeagueException e) {
-            Assert.assertEquals(CricketLeagueException.ExceptionType.HEADER_INCORRECT,e.type);
+            Assert.assertEquals(CricketLeagueException.ExceptionType.HEADER_INCORRECT, e.type);
         }
-        }
+    }
 
     @Test
     public void givenIPLFile_ShouldSortTheData_AndGiveTopBattingAverage() {
-        CricketLeagueAnalyser cricketLeagueAnalyser=new CricketLeagueAnalyser();
+        CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
         try {
             cricketLeagueAnalyser.getCount(RUNS_FILE);
-            String sortedList=cricketLeagueAnalyser.getBattingAverage();
+            String sortedList = cricketLeagueAnalyser.getBattingAverage();
             Batsman[] censusCSV = new Gson().fromJson(sortedList, Batsman[].class);
-            Assert.assertEquals("MS Dhoni",censusCSV[100].player);
+            Assert.assertEquals("MS Dhoni", censusCSV[100].player);
         } catch (CricketLeagueException e) {
             e.printStackTrace();
         }
+    }
 
+    @Test
+    public void givenIPLFile_ShouldSortTheData_AndGivePLayerWithHigh_StrikingRate() {
+        CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
+        try {
+            cricketLeagueAnalyser.getCount(RUNS_FILE);
+            String sortedList = cricketLeagueAnalyser.getStrikingRate();
+            Batsman[] censusCSV = new Gson().fromJson(sortedList, Batsman[].class);
+            Assert.assertEquals("Ishant Sharma", censusCSV[100].player);
+        } catch (CricketLeagueException e) {
+            e.printStackTrace();
+        }
     }
 }
 
