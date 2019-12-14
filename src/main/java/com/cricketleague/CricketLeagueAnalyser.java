eb75ac724a9f4aa489e2f6d9f317e17377e8b0a7
janhavi07 <partejanhavi@gmail.com>
2019-12-14 17:41:26 +0530

    Added code to sorting of the data according to average batting rate

--- src/main/java/com/cricketleague/CricketLeagueAnalyser.java
+++ src/main/java/com/cricketleague/CricketLeagueAnalyser.java
@@ -3,30 +3,40 @@ package com.cricketleague;
 import com.csvReader.CSVBuilderException;
 import com.csvReader.CSVBuilderFactory;
 import com.csvReader.ICSVBuilder;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 public class CricketLeagueAnalyser {
-    ArrayList<Batsman> batsmanArrayList;
+    List<Batsman> batsmanList=new ArrayList<>();
 
     public int getCount(String runsFile) throws CricketLeagueException {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(runsFile));
             ICSVBuilder icsvBuilder= CSVBuilderFactory.createCSVBuilder();
-            return icsvBuilder.getCSVFileList(reader,Batsman.class).size();
+            batsmanList=icsvBuilder.getCSVFileList(reader,Batsman.class);
+            return batsmanList.size();
         } catch (IOException e) {
             throw new CricketLeagueException(CricketLeagueException.ExceptionType.FILE_PROBLEM,"Wrong file inputted");
         } catch (CSVBuilderException e) {
            throw new CricketLeagueException(CricketLeagueException.ExceptionType.BUILDER_EXCEPTION,e.getMessage());
         } catch (RuntimeException e){
-            throw new CricketLeagueException(CricketLeagueException.ExceptionType.HEADER_INCORRECT,e.getMessage());
+            throw new CricketLeagueException(CricketLeagueException.ExceptionType.HEADER_INCORRECT,"Incorrect Header or Delimiter");
         }
+    }
+
+    public String getBattingAverage() {
+        Comparator<Batsman> comparator =Comparator.comparing(player -> player.average);
+        ArrayList c=batsmanList.stream()
+                 .sorted(comparator)
+                .collect(Collectors.toCollection(ArrayList::new));
+        String sortedData=new Gson().toJson(c);
+        return sortedData;
     }
 }
