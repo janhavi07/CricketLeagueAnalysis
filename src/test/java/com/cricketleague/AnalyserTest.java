2cda949546e1f68a128a72c2e0a97ed3db4c00da
janhavi07 <partejanhavi@gmail.com>
2019-12-14 14:29:29 +0530

    Added test code to get the total count to ensure loading of csvfile

--- src/test/java/com/cricketleague/AnalyserTest.java
+++ src/test/java/com/cricketleague/AnalyserTest.java
@@ -1,4 +1,17 @@
 package com.cricketleague;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class AnalyserTest {
+
+    private static final String RUNS_FILE="./src/test/resources/IPL2019FactsheetMostRuns.csv";
+
+    @Test
+    public void givenTheRunsFile_ShouldReturnTheTotalCountOfProgram() {
+        CricketLeagueAnalyser cricketLeagueAnalyser=new CricketLeagueAnalyser();
+        int getCount=cricketLeagueAnalyser.getCount(RUNS_FILE);
+        Assert.assertEquals(101,getCount);
+
+    }
 }
