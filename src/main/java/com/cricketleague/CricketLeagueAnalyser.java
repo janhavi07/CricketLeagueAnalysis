c8f3a308dfd5ee6158fbadbb35d56a126f84dd8f
janhavi07 <partejanhavi@gmail.com>
2019-12-14 15:42:58 +0530

    Added code to catch incorrect delimeter exception

--- src/main/java/com/cricketleague/CricketLeagueAnalyser.java
+++ src/main/java/com/cricketleague/CricketLeagueAnalyser.java
@@ -8,25 +8,25 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.StreamSupport;
 
 public class CricketLeagueAnalyser {
+    ArrayList<Batsman> batsmanArrayList;
 
     public int getCount(String runsFile) throws CricketLeagueException {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(runsFile));
             ICSVBuilder icsvBuilder= CSVBuilderFactory.createCSVBuilder();
-            Iterator<Batsman> csvFileIterator = icsvBuilder.getCSVFileIterator(reader,Batsman.class);
-            Iterable<Batsman> csvIterable = () -> csvFileIterator;
-            int namOfEateries = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
-            return namOfEateries;
+            return icsvBuilder.getCSVFileList(reader,Batsman.class).size();
         } catch (IOException e) {
             throw new CricketLeagueException(CricketLeagueException.ExceptionType.FILE_PROBLEM,"Wrong file inputted");
         } catch (CSVBuilderException e) {
-            e.printStackTrace();
+           throw new CricketLeagueException(CricketLeagueException.ExceptionType.BUILDER_EXCEPTION,e.getMessage());
+        } catch (RuntimeException e){
+            throw new CricketLeagueException(CricketLeagueException.ExceptionType.HEADER_INCORRECT,e.getMessage());
         }
-        return 0;
     }
 }
