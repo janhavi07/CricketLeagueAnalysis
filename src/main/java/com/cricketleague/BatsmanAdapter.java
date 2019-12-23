e7e34f36a0439f6efcc8fbce2377a839fc170340
janhavi07 <partejanhavi@gmail.com>
2019-12-23 11:12:45 +0530

    Added batsaman adapter to implement adapter methods with respective class files

--- src/main/java/com/cricketleague/BatsmanAdapter.java
+++ src/main/java/com/cricketleague/BatsmanAdapter.java
@@ -1,4 +1,11 @@
 package com.cricketleague;
 
-public class BatsmanAdapter {
+import java.util.Map;
+
+public class BatsmanAdapter extends IPLAdapter {
+    @Override
+    protected <E> Map<String, IPLDao> loadPLayerData(String csvFile) throws CricketLeagueException {
+        Map<String, IPLDao> iplList=super.loadData(Batsman.class,csvFile);
+        return iplList;
+    }
 }
