93dd3aff0f69d5f42b245a68000fbcc54bfa6ede
janhavi07 <partejanhavi@gmail.com>
2019-12-23 11:11:44 +0530

    Added an abstract adapter class to load the player info

---
+++ src/main/java/com/cricketleague/BatsmanAdapter.java
@@ -0,0 +1,4 @@
+package com.cricketleague;
+
+public class BatsmanAdapter {
+}
---
+++ src/main/java/com/cricketleague/IPLAdapter.java
@@ -0,0 +1,43 @@
+package com.cricketleague;
+
+import com.csvReader.CSVBuilderException;
+import com.csvReader.CSVBuilderFactory;
+import com.csvReader.ICSVBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.stream.StreamSupport;
+
+public abstract class IPLAdapter {
+    protected abstract <E> Map<String,IPLDao> loadPLayerData(String csvFile) throws CricketLeagueException;
+
+    public <E> Map<String,IPLDao> loadData(Class<E> csvClass,String csvFile) throws CricketLeagueException {
+        try {
+            Map<String,IPLDao> iplList=new HashMap<>();
+            Reader reader = Files.newBufferedReader(Paths.get(csvFile));
+            ICSVBuilder icsvBuilder = CSVBuilderFactory.createCSVBuilder();
+            Iterator<E> csvIterator = icsvBuilder.getCSVFileIterator(reader, csvClass);
+            Iterable<E> Iterable = () -> csvIterator;
+            if (csvClass.getName().equals("cricketleague.Batsman")){
+            StreamSupport.stream(Iterable.spliterator(), false).
+                    map(Batsman.class::cast).
+                    forEach(iplCSV -> iplList.put(iplCSV.player, new IPLDao(iplCSV)));}
+            else if(csvClass.getName().equals("cricketleague.Bowlers")){
+                StreamSupport.stream(Iterable.spliterator(), false).
+                        map(Bowlers.class::cast).
+                        forEach(iplCSV -> iplList.put(iplCSV.player, new IPLDao(iplCSV)));}
+            return iplList;
+        } catch (IOException e) {
+            throw new CricketLeagueException(CricketLeagueException.ExceptionType.FILE_PROBLEM, "Wrong file inputted");
+        } catch (CSVBuilderException e) {
+            throw new CricketLeagueException(CricketLeagueException.ExceptionType.BUILDER_EXCEPTION, e.getMessage());
+        } catch (RuntimeException e) {
+            throw new CricketLeagueException(CricketLeagueException.ExceptionType.HEADER_INCORRECT, "Incorrect Header or Delimiter");
+        }
+    }
+}
