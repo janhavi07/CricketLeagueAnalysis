66a5f69a2550196521da27918b504d0225564d19
janhavi07 <partejanhavi@gmail.com>
2019-12-23 11:13:42 +0530

    Added bowler adapter class to implement adapter method to load bowler csv file

--- src/main/java/com/cricketleague/BowlerAdapter.java
+++ src/main/java/com/cricketleague/BowlerAdapter.java
@@ -1,4 +1,11 @@
 package com.cricketleague;
 
-public class BowlerAdapter {
+import java.util.Map;
+
+public class BowlerAdapter extends IPLAdapter {
+    @Override
+    protected <E> Map<String, IPLDao> loadPLayerData(String csvFile) throws CricketLeagueException {
+        Map<String, IPLDao> iplList=super.loadData(Bowlers.class,csvFile);
+        return iplList;
+    }
 }
